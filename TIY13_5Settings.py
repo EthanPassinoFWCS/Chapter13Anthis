26ee013ff83cedecc7de28ef293894e568009780
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-10-19 15:17:07 -0400

    Started TIY 5...

---
+++ TIY13_5Settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    def __init__(self):
+        # Screen Settings
+        self.screen_width = 1280 # sets screen width VARIABLE.
+        self.screen_height = 720 # sets screen height VARIABLE.
+        self.bg_color = (230, 230, 230) # sets background color VARIABLE.
+        # This can also be used to make an options file that is editable in-game by the user with the use of json.
+        # To use this, import it in the main game.
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 1.5 # 1 speed
+        self.bullet_width = 15 # 15 pixels wide
+        self.bullet_height = 3 # 3 pixels high
+        self.bullet_color = (60, 60, 60) # dark gray
+        self.bullets_allowed = 3
+
+        # Alien Settings
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 10
+        # fleet direction, 1 represents right; -1 represents left.
+        self.fleet_direction = 1
